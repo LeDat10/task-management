83408ac009e83c9bc4783598890d616d59e0674f
ledat <ledat1010204@gmail.com>
2025-03-03 15:12:32 +0700

    Tạo routes

--- index.js
+++ index.js
@@ -3,38 +3,15 @@ require("dotenv").config();
 
 const database = require("./config/database");
 const Task = require("./models/task.model");
+const routesApiVer1 = require("./api/v1/routes/index.route");
 
 const app = express();
 const port = process.env.PORT;
 
 database.connect();
 
-app.get("/tasks", async (req, res) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
-
-    console.log(tasks);
-
-    res.json(tasks);
-});
-
-app.get("/tasks/detail/:id", async (req, res) => {
-    try {
-        const id = req.params.id;
-
-        const task = await Task.findOne({
-            _id: id,
-            deleted: false
-        });
-
-        res.json(task);
-
-    } catch (error) {
-        res.json("Không tìm thấy!");
-    }
-
-});
+// Routes version 1
+routesApiVer1(app);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
